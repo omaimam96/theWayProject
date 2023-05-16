5278c988be607b1f7cffe24665d97977f2b88c94
omaima <o.chouchane@contrader.it>
2023-05-16 21:31:22 +0200

    edits

---
+++ src/test/java/com/example/theWay/TheWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
